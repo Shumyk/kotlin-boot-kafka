2d3875a1db543422cdc954494572c24f43aaa7ad
Shumyk <dima.shumyk@gmail.com>
2023-03-21 23:08:13 +0200

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,18 @@
 FROM openjdk:17-alpine
 
+RUN apk update \
+    && apk upgrade \
+    && apk --no-cache add curl openssl perl
+
+COPY import_rds_certs.sh /certs/import_rds_certs.sh
+ARG trustStorePassword
+RUN /certs/import_rds_certs.sh ${trustStorePassword}
+
+ENV TRUST_STORE_PASSWORD=${trustStorePassword}
+
 COPY build/libs/kafka-worker*.jar KafkaService.jar
 
-ENTRYPOINT ["java", "-jar", "KafkaService.jar"]
+CMD java -jar \
+    -Djavax.net.ssl.trustStore=/certs/rds-truststore.jks \
+    -Djavax.net.ssl.trustStorePassword=${TRUST_STORE_PASSWORD} \
+    KafkaService.jar
---
+++ import_rds_certs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# first argument is the trustStorePassword
+
+DIR=/certs
+TRUSTSTORE=${DIR}/rds-truststore.jks
+# password from argument
+STORE_PASSWORD=$1
+
+curl -sS "https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem" > ${DIR}/rds-combined-ca-bundle.pem
+awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1}{print > "rds-ca-" n ".pem"}' < ${DIR}/rds-combined-ca-bundle.pem
+
+for CERT in rds-ca-*; do
+  ALIAS=$(openssl x509 -noout -text -in $CERT | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
+  echo "Importing $ALIAS"
+  keytool -import -file ${CERT} -alias "${ALIAS}" -storepass ${STORE_PASSWORD} -keystore ${TRUSTSTORE} -noprompt
+  rm ${CERT}
+done
+
+rm ${DIR}/rds-combined-ca-bundle.pem
